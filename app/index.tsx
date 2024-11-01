6f6cb4832b8fa6d58001838392c3a5327621c4a4
Taofeeq <oyelowomayowa@gmail.com>
2024-11-01 23:21:13 +0100

    Nativewind setup

--- app/index.tsx
+++ app/index.tsx
@@ -3,7 +3,9 @@ import { Text, View } from "react-native";
 export default function HomeScreen() {
   return (
     <View>
-      <Text>Home Screen</Text>
+      <Text className="font-bold text-5xl p-10 bg-red-500 text-blue-700">
+        Home Screen
+      </Text>
     </View>
   );
 }
